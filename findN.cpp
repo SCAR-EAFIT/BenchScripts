971a35232528690bce3384004865295756fae15d
JuanM0412 <juangomezp1204@gmail.com>
2023-12-07 11:31:13 -0500

    Format

--- findN.cpp
+++ findN.cpp
@@ -8,6 +8,7 @@ using namespace std;
 unsigned long get_mem_total() {
     string token;
     ifstream file("/proc/meminfo");
+
     while(file >> token){
         if(token == "MemTotal:"){
             unsigned long mem;
@@ -18,9 +19,9 @@ unsigned long get_mem_total() {
                 return 0;
             }
         }
-        // Ignore the rest of the line
         file.ignore(numeric_limits<streamsize>::max(), '\n');
     }
+
     return 0;
 }
 
@@ -29,6 +30,7 @@ double findN(double mem, double nodes, double perc, double NB_value){
     totalmemory = trunc(totalmemory * pow(2, 20));
     double toplimit = perc * trunc(sqrt(totalmemory / 8));
     double quotient = trunc(toplimit / NB_value);
+
 	return quotient * NB_value;
 }
 
@@ -36,10 +38,10 @@ int main() {
     unsigned long mem_total_kib = get_mem_total();
     unsigned long mem_total_bytes = mem_total_kib * 1024;
     double mem_total_megabytes = mem_total_bytes / (1024.0 * 1024.0);
+    double NB[] = {128, 256, 384, 512, 640, 768, 896, 1024};
+
     cout << "Memory size: " << mem_total_megabytes << " MB" << endl;
     cout << endl;
-	
-	double NB[] = {128, 256, 384, 512, 640, 768, 896, 1024};
 
 	for(auto NB_value: NB){
 		cout << "NB: " << NB_value << endl;
