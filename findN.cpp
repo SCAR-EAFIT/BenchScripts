da9736f81e2e6f17f8a170af7af84c4f9761691d
JuanM0412 <juangomezp1204@gmail.com>
2023-12-07 11:30:15 -0500

    Exact memory value and formula for N

--- findN.cpp
+++ findN.cpp
@@ -1,32 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include <limits>
+#include <cmath>
+
+using namespace std;
 
 unsigned long get_mem_total() {
-    std::string token;
-    std::ifstream file("/proc/meminfo");
-    while (file >> token) {
-        if (token == "MemTotal:") {
+    string token;
+    ifstream file("/proc/meminfo");
+    while(file >> token){
+        if(token == "MemTotal:"){
             unsigned long mem;
-            if (file >> mem) {
+            if(file >> mem){
                 return mem;
-            } else {
+            } 
+			else{
                 return 0;
             }
         }
         // Ignore the rest of the line
-        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        file.ignore(numeric_limits<streamsize>::max(), '\n');
     }
-    return 0; // Nothing found
+    return 0;
+}
+
+double findN(double mem, double nodes, double perc, double NB_value){
+	double totalmemory = mem * nodes;
+    totalmemory = trunc(totalmemory * pow(2, 20));
+    double toplimit = perc * trunc(sqrt(totalmemory / 8));
+    double quotient = trunc(toplimit / NB_value);
+	return quotient * NB_value;
 }
 
 int main() {
-    unsigned long mem = get_mem_total();
-    // Convert to gibibytes (1 GiB = 1024^3 kibibytes)
-    double mem_gib = static_cast<double>(mem) / (1024 * 1024);
-    double mem_gb = mem_gib * 1.07374;
-    std::cout << "Memory size: " << mem_gb << " GB" << std::endl;
-    std::cout << "Memory size: " << mem_gib << " GiB" << std::endl;
+    unsigned long mem_total_kib = get_mem_total();
+    unsigned long mem_total_bytes = mem_total_kib * 1024;
+    double mem_total_megabytes = mem_total_bytes / (1024.0 * 1024.0);
+    cout << "Memory size: " << mem_total_megabytes << " MB" << endl;
+    cout << endl;
+	
+	double NB[] = {128, 256, 384, 512, 640, 768, 896, 1024};
+
+	for(auto NB_value: NB){
+		cout << "NB: " << NB_value << endl;
+		for(double i = 0.90; i <= 1; i+=0.01){
+			cout << findN(mem_total_megabytes, 1, i, NB_value) << " ";
+		}
+		cout << endl << endl;
+	}
 
     return 0;
 }
